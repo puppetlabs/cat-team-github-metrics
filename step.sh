eb21178da5340df6ad609ded1c0c518411b5219b
Craig Gumbley <craiggumbley@gmail.com>
2022-07-02 21:04:24 +0100

    Fix GCP credentials
    At some point the credential set up step was removed from step.sh.
    
    This commit adds it back in to the mix.

--- step.sh
+++ step.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo $(ni get -p {.connection.serviceAccountKey}) > /app/credentials.json
+
+export GOOGLE_APPLICATION_CREDENTIALS="credentials.json"
 export GITHUB_TOKEN=$(ni get -p {.github_token})
 export BIG_QUERY_PROJECT_ID=$(ni get -p {.big_query_project_id})
 export REPO_OWNER=$(ni get -p {.repo_owner})
