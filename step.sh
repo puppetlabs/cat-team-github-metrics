c18a8711f7b93fd47ac1205bba9da66d2dd5eb77
jordanbreen28 <jordan.breen@perforce.com>
2022-11-30 14:53:09 +0000

    (CONT-275) - Add metrics workflow
    Prior to this commit, the grafana dashboard we use to display github
    metrics relied on relay for orchestrating the data.
    As relay is being turned off, this commit includes a github action that
    will take over this role and perform the same actions as the relay
    workflow.

--- step.sh
+++ step.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo $(ni get -p {.connection.serviceAccountKey}) > /app/credentials.json
+echo $CONNECTION_KEY > /app/credentials.json
 
-export GOOGLE_APPLICATION_CREDENTIALS="credentials.json"
-export GITHUB_TOKEN=$(ni get -p {.github_token})
-export BIG_QUERY_PROJECT_ID=$(ni get -p {.big_query_project_id})
-export REPO_OWNER=$(ni get -p {.repo_owner})
-export REPO_NAME=$(ni get -p {.repo_name})
+export GOOGLE_APPLICATION_CREDENTIALS="/app/credentials.json"
+export GITHUB_TOKEN="$GITHUB_TOKEN"
+export BIG_QUERY_PROJECT_ID="$BQ_PROJECT_ID"
+export REPO_OWNER="$REPO_OWNER"
+export REPO_NAME="$REPO_NAME"
 
-COMMAND=$(ni get -p {.command})
+COMMAND="$COBRA_COMMAND"
 
-case $COMMAND in
+case "$COMMAND" in
     export|stamp) ./collector $COMMAND;;
     *) echo "Invalid command! Should be one of [export, stamp]." && exit 1;;
 esac
